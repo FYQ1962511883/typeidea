e1873a3ce636afaf598d98800861b39d909881ca
FYQ1962511883 <1962511883@qq.com>
2020-01-06 13:16:11 +0800

    2020/1/6/13:16

--- typeidea/urls.py
+++ typeidea/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.sitemaps import views as sitemap_views
 from django.conf.urls import url
 from django.contrib import admin
 
+from blog.rss import LatestPostFeed
+from blog.sitemap import PostSitemap
 from blog.views import (
     PostDetailView, IndexView, CategoryView,
     TagView, SearchView,AuthorView
@@ -26,6 +29,8 @@ from typeidea.custom_site import custom_site
 
 
 urlpatterns = [
+    url(r'^rss|feed/', LatestPostFeed(), name='rss'),
+    url(r'^sitemap\.xml$', sitemap_views.sitemap, {'sitemaps': {'posts': PostSitemap}}),
     url(r'^comment/$', CommentView.as_view(), name='comment'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
     url(r'^search/$', SearchView.as_view(), name='search'),
